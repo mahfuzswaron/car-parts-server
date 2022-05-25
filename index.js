c3d564635e7c212677522c1aa922c6c1eab86ec1
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-25 23:19:22 +0600

    loading single product by id done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -27,9 +27,13 @@ async function run() {
     app.get('/parts', async (req, res) => {
       const result = await partsCollection.find({}).toArray();
       res.send(result);
+    })
 
-
-
+    // get single product
+    app.get('/parts/:id', async (req, res) => {
+      const id = ObjectId(req.params.id);
+      const result = await partsCollection.findOne({ _id: id });
+      res.send(result)
     })
 
   }
